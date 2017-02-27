1d9eeec9762abda8fb85b3d9f444cd07d5c7de59
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-26 21:16:23 -0500

    added column,'directions' to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227012811) do
+ActiveRecord::Schema.define(version: 20170227020908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170227012811) do
     t.string   "ingredients"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "directions"
   end
 
 end
