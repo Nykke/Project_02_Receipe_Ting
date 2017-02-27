0b4707024b659e3c2b62180c3115b90457d0cd3c
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-02-27 10:14:07 -0500

    added recipe data

---
+++ db/recipe_data.rb
@@ -0,0 +1,231 @@
+all recipes = [
+  {
+  "id": "001",
+  "title": "Ginger Beer",
+  "img_url": "http://www.seriouseats.com/recipes/assets_c/2012/01/20120130-189715-ginger-beer-thumb-625xauto-214181.jpg",
+  "ingredients": [
+    "1 1/2 lb green ginger",
+    "4 lb brown sugar",
+    "1 1/2 gallons water",
+    "1 lemon sliced",
+    "handful of cinnamon",
+    "handful of cloves",
+    "handful of rice"
+  ],
+  "directions": {
+    "Step 1": "Grate ginger.",
+    "Step 2": "Add sugar to water and stir until dissolved.",
+    "Step 3": "Pour mixture onto grated ginger.",
+    "Step 4": "Add lemon slices, cinnamon, cloves and rice.",
+    "Step 5": "Cover and leave for three days.",
+    "Step 6": "Strain mixture and enjoy."
+  },
+  "sources": [
+    "www.seriouseats.com",
+    "What's Cooking in Guyana by Carnegie School of Home Economics"
+  ]
+},
+{
+  "id": "002",
+  "title": "Mauby",
+  "img_url": "https://s-media-cache-ak0.pinimg.com/originals/ec/0e/6a/ec0e6a105d32a09163c810bf250b8060.jpg",
+  "ingredients": [
+    "1/2 oz mauby bark",
+    "2 pieces of cinnamon spice",
+    "6 pints water",
+    "small piece dried orange peel",
+    "3-4 cloves",
+    "2 lb sugar"
+  ],
+  "directions": {
+    "Step 1": "Boil mauby bark, orange peel, cinnamon spice and cloves, in one cup of water for ten minutes.",
+    "Step 2": "Cool and add to water and sugar.",
+    "Step 3": "Brew for about five minutes.",
+    "Step 4": "Fill bottles and leave for two days.",
+    "Step 5": "Enjoy."
+  },
+    "sources": [
+      "wwww.pinterest.com",
+      "What's Cooking in Guyana by Carnegie School of Home Economics"
+    ]
+  },
+  {
+    "id": "003",
+    "title": "Vanilla Milkshake",
+    "img_url": "https://search.chow.com/thumbnail/800/600/www.chowstatic.com/assets/2015/02/29737_old_fashioned_milkshake.jpg",
+    "ingredients": [
+      "4 cups vanilla ice cream",
+      "2 teaspoons vanilla extract",
+      "8 tablespoons sugar",
+      "2 cups milk"
+    ],
+    "directions": {
+      "Step 1": "Blend all ingredients together until smooth.",
+      "Step 2": "Enjoy."
+    },
+    "sources": [
+      "http://www.foodnetwork.com/recipes/paula-deen/vanilla-milkshake-recipe"
+    ]
+  },
+  {
+    "id": "004",
+    "title": "Traditional Sweet Iced Tea",
+    "img_url": "https://pioneerwoman.files.wordpress.com/2016/05/how-to-make-sweet-tea-01.jpg",
+    "ingredients": [
+      "6 tea bags",
+      "1/6 teaspoon baking soda",
+      "2 cups hot water",
+      "2 cups sugar",
+      "6 cups cold water"
+    ],
+    "directions": {
+      "Step 1": "Place tea bags and baking soda in a glass bowl and pour the hot(boiling) water over them.",
+      "Step 2": "Take the tea bags out but do not squeeze them.",
+      "Step 3": "Pour the tea mixture into a pitcher; add sugar.",
+      "Step 4": "Stir sugar is dissolved then add the cold water.",
+      "Step 5": "Let cool and enjoy."
+    },
+    "sources": [
+      "http://www.food.com/recipe/southern-sweet-iced-tea-63785"
+    ]
+  },
+  {
+    "id": "005",
+    "title": "Cheese Straws",
+    "img_url": "http://4.bp.blogspot.com/-3kNJ2t32cds/URfhDx0WvjI/AAAAAAAAG00/6FfEg4L82xc/s1600/IMG_8557.JPG",
+    "ingredients": [
+      "1 3/4 lb flour",
+      "1 lb margarine",
+      "1 lb grated cheese",
+      "1/2 tsp mustard",
+      "1/4 tsp pepper"
+    ],
+    "directions": {
+      "Step 1": "Rub margarine into flour.",
+      "Step 2": "Add grated cheese, mustard and pepper and mix thoroughly.",
+      "Step 3": "Using a forcing bag and star tube or the star disc and a cookie biscuit maker, pipe on the baking sheets and bake in a hot oven for 7-10 minutes.",
+      "Step 4": "When cool, break into required lengths."
+    },
+    "sources": [
+      "What's Cooking in Guyana by Carnegie School of Home Economics"
+    ]
+  },
+  {
+    "id": "006",
+    "title": "Mini Burgers",
+    "img_url": "http://bonappetitcrepecompany.com/wp-content/uploads/2016/04/mini-burger-4.jpg",
+    "ingredients":[
+      "1 lb ground chuck",
+      "1/4 lb boneless short rib, minced",
+      "6 mini brioche buns, split and toasted",
+      "Ketchup and pickle slices, for serving"
+    ],
+    "directions": {
+      "Step 1": "Light a grill.",
+      "Step 2": "Combine the chuck and short rib; season with salt and pepper.",
+      "Step 3": "Form the meat into six patties.",
+      "Step 4": "Grill over high heat, turn once, about six minutes for medium-rare.",
+      "Step 5": "Place burgers on the toasted buns, top with ketchup and pickles"
+    },
+    "sources": [
+      "http://www.foodandwine.com/recipes/mini-burgers-crispy-onion-rings"
+    ]
+  },
+  {
+    "id": "007",
+    "title": "Prawn Curry",
+    "img_url": "http://www.wikihow.com/images/5/57/Make-Prawn-Curry-Intro.jpg",
+    "ingredients": [
+      "1 lb prawns",
+      "1 small coconut",
+      "1/4 lb onion",
+      "3 cloves garlic",
+      "2 tsb gheera",
+      "2 bay leaves",
+      "salt to taste",
+      "1 tbs ghee",
+      "1/4 pint oil",
+      "pepper to taste",
+      "2 tsp tumeric",
+      "1/2 oz curry powder"
+    ],
+    "directions": {
+      "Step 1": "Grate coconut. Set aside 2 tbsp of it. Add 1/2 pint water to rest of grated coconut and squeeze out milk.",
+      "Step 2": "Chop onions and garlic, put oil in pot and fry lightly with bay leaves.",
+      "Step 3": "Add tumeric, curry powder and gheera and fry for a minute.",
+      "Step 4": "Add prawns and grated coconut, salt and pepper. Fry for a few minutes.",
+      "Step 5": "Add coconut milk, stir and cover pot. Boil on medium heat until prawns are cooked, about five minutes.",
+      "Step 6": "Add ghee before removing from heat.",
+      "Step 7": "Serve hot with rice, sprinkled with rest of grated coconut."
+    },
+    "sources": [
+      "What's Cooking in Guyana by Carnegie School of Home Economics"
+    ]
+  },
+  {
+    "id": "008",
+    "title": "Meatloaf",
+    "img_url": "http://pictures.food.com/api/file/uTzl5N1wSE68ETtWNRZe-026-virginia-meatloaf.jpg/convert?loc=/pictures.food.com/recipes/54/25/7/HykjhqkxTXSRGyHcJAoM_026%20virginia%20meatloaf.jpg&width=614&height=461&fit=crop",
+    "ingredients": [
+      "1 1/2 lbs ground beef",
+      "2 ounce envelope onion soup mix",
+      "1 cup shredded Italian cheese blend",
+      "2 eggs",
+      "1 1/2 cups saltines (4 oz), finely ground",
+      "1/3 cup water",
+      "1/2 cup barbeque sauce"
+    ],
+    "directions": {
+      "Step 1": "Heat oven to 400° F. In a large bowl, combine the beef, soup mix, cheese, eggs, and saltines; add water and mix.",
+      "Step 2": "Transfer the meat mixture to a baking dish and form it into a 9-inch loaf. Bake until cooked through, about 40 minutes.",
+      "Step 3": "Remove the meat loaf from oven after 25 minutes and pour the sauce over the top. Return to oven for about 15 minutes more."
+    },
+    "sources": [
+      "http://www.foodandwine.com/recipes/moms-meat-loaf"
+    ]
+  },
+  {
+    "id": "009",
+    "title": "Homemade Ice Cream",
+    "img_url": "http://getinspiredeveryday.com/food/wp-content/uploads/sites/5/2013/11/Homemade-Maple-Ice-Cream-GI-365-61.jpg",
+    "ingredients": [
+      "1 pint milk",
+      "2 tbsp cornstarch or custard powder",
+      "3 eggs",
+      "2 oz sugar",
+      "flavoring (if desired)"
+    ],
+    "directions": {
+      "Step 1": "Blend custard powder with a little cold milk.",
+      "Step 2": "Heat the rest of the milk and add to blended custard powder.",
+      "Step 3": "Bring to boil and let it boil for about 3 minutes. Remove from the heat.",
+      "Step 4": "Add boiled mixture to beaten egg and sugar immediately, stirring all the time. Add flavoring.",
+      "Step 5": "Cool and Freeze.",
+      "Step 6": "Enjoy."
+    },
+    "sources": [
+      "What's Cooking in Guyana by Carnegie School of Home Economics"
+    ]
+  },
+  {
+    "id": "010",
+    "title": "Peach Cobbler",
+    "img_url": "https://images-gmi-pmc.edge-generalmills.com/3091046d-6fa8-47f6-baad-0c36a14b5279.jpg",
+    "ingredients": [
+      "2 1/2 lbs peach, cut into 1/2 inch thick wedges",
+      "1/2 cup sugar",
+      "1 1/4 cups all purpose flour, spooned and leveled",
+      "6 tablespoons cold unsalted butter, cut into pieces",
+      "1 teaspoon baking powder",
+      "1/4 teaspoon kosher salt"
+    ],
+    "directions": {
+      "Step 1": "Heat oven to 350° F. In a bowl, toss the peaches with ¼ cup of the sugar and 2 tablespoons of the flour; transfer to an 8-inch square baking dish.",
+      "Step 2": "In a food processor, combine the remaining 1¼ cups flour and ¼ cup sugar with the butter, baking powder, and salt. Pulse until coarse crumbs form. Add the cream and pulse just until moistened (the dough will be slightly shaggy). Drop spoonfuls of the batter onto the peaches.",
+      "Step 3": "Place the baking dish on a rimmed baking sheet and bake until golden brown and bubbly, 50 to 60 minutes. Serve with vanilla ice cream, if desired."
+    },
+    "sources": [
+      "http://www.realsimple.com/food-recipes/browse-all-recipes/peach-cobbler"
+    ]
+  }
+  ]
