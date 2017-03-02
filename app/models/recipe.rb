7bbe122f22c26c81c83b70f03df886719bb574b7
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-02 00:46:27 -0500

    validations for forms started, incomplete

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :comments
+
+  # validates :title, :ingredients, :img_url, :directions, :presence => true
 end
